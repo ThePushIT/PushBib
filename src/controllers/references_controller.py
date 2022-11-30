700987a2fbe68cbe0cf6063b41005850bcdbf80f
Olivia Horjamo <olivia.horjamo@gmail.com>
2022-11-30 15:58:30 +0200

    Add post method for adding a book

--- src/controllers/references_controller.py
+++ src/controllers/references_controller.py
@@ -1,15 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, request
 
 ref_controller = Blueprint("ref", __name__)
 
-@ref_controller.route('/references/')
-def show_references():
+@ref_controller.route('/references/<id>')
+def show_references(id):
+    user_id = id
     # find the user in case
     # find all the references by this user
-    # put to the template
-
-    # on top of the template a form for adding a new reference
+    # give as a parameter for the template
 
     #references = reference_service.get_all_references(user_id)
 
-    return "Welcome to references"
+    return render_template('references.html', user = user_id)
+
+@ref_controller.route('/references/add/<id>', methods = ['POST'])
+def add_reference(id):
+    user_id = id
+    name = request.form.get('name')
+    authors = request.form.get('authors')
+    year = request.form.get('year')
+    print(name, authors, year, user_id)
+
+    return redirect(f'/references/{id}')
