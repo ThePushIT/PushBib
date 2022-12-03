1bccbf93ddfd5a072313585c17451fd4d814bc95
Olivia Horjamo <olivia.horjamo@gmail.com>
2022-12-03 14:15:02 +0200

    Unittest kirjan lisäämisen onnistumiselle

--- src/tests/repositories/reference_repository_test.py
+++ src/tests/repositories/reference_repository_test.py
@@ -3,21 +3,25 @@ from repositories.reference_repository import reference_repository
 from repositories.user_repository import user_repository
 from services.user_service import user_service
 from services.reference_service import reference_service
-from unittest.mock import patch
-from app import app
 
 
 class TestReferenceRepository(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # ajetaan ennen kaikkia testitapauksia
+        # tyhjennetään testitietokanta käyttäjistä jotta id:t oikein
+        # luodaan testikäyttäjä jolle voidaan lisätä kirjoja
+        user_repository.delete_all_users()
+        user_repository.create('testuser', 'testpassword')
+
     def setUp(self):
+        # ajetaan ennen jokaista testiä
         reference_repository.delete_all_books()
 
     def test_insert_book_reference_succeeds(self):
-        # ongelmat tällä hetkellä
-        # testi tarvis session mut testitapaukset ei hyväksy html viestintää
-        # tän takia rekisteröinti ei onnistu
-        # jos rekisteröinti ei onnistu niin kirjan lisääminen ei onnistu
-        #user_service.register('olivia', 'jeejeejee')
-        self.assertTrue(
-            reference_repository.insert_book_reference(
-                1, "Anonyymi", "Kiva kirja", 2020, "Otava")
-        )
+        reference_repository.insert_book_reference(
+                            '1', "Anonyymi", "Kiva kirja", 2020, "Otava")
+        books = reference_repository.fetch_all_references(1)
+        print(books)
+        self.assertEqual(1, len(books))
+
