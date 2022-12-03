d5942b65b70cb57bb5b1a69c0e1afd8a2878ddc5
Olivia Horjamo <olivia.horjamo@gmail.com>
2022-12-03 13:28:04 +0200

    Add to-do descriptions

--- src/controllers/references_controller.py
+++ src/controllers/references_controller.py
@@ -8,6 +8,12 @@ ref_controller = Blueprint("ref", __name__)
 @ref_controller.route('/references/<user_id>')
 def show_references(user_id):
     # change to accommodate all types of references, not just books
+
+    # TODO
+    # poista user id routesta ja sen sijaan hae tässä kohtaa käyttäjän id
+    # id = user_service.get_id() tyyliin
+    # hae tämän id:n perusteella referencet
+
     references = reference_service.get_references(int(user_id))
 
     return render_template('references.html', user_id=user_id,
@@ -15,6 +21,12 @@ def show_references(user_id):
 
 @ref_controller.route('/references/add/<user_id>', methods=['POST'])
 def add_book(user_id):
+
+    # TODO
+    # poista user id routesta ja sen sijaan hae tässä kohtaa käyttäjän id
+    # id = user_service.get_id() tyyliin
+    # hae tämän id:n perusteella referencet
+
     authors = request.form.get('authors')
     title = request.form.get('title')
     year = request.form.get('year')
